d5e5238c4ab9b3d929b5aa835a9542cb53cc743c
Norman <norman-rdd@mail.ru>
2021-06-10 16:09:34 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,110 @@
+"use strict";
+
+
+/*
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log (number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+console.log (obj);
+
+var name = "Ivan";
+console.log(name);
+
+{
+    var result = 50;  //к let и const нельзя обратится вне блока 
+}
+console.log(result);
+
+alert('Если вы гей, нажимите окей');
+[].push('a');
+
+
+let number = 4.6;
+
+console.log (-4/0) ; 
+console.log ('string' * 9);
+
+const persone = `5`;
+const bool = false;
+
+// console.log(something);
+
+let und;
+console.log(und);
+*/
+
+/*
+const obj = {
+    name: "Norman",
+    age: 25,
+    isMarried: false    
+};
+
+    // console.log(obj.name);
+    console.log(obj["name"]);
+    let arr = ['plum.png','orange.jpg',6,'apple.bmp',{},[]];
+    console.log(arr[1]);
+    */
+//    alert('Hello');
+// const result = confirm("Are u here?");
+// console.log(result);
+// const answer = prompt ("Вам есть 18?","17");
+// const answer = +prompt ("Вам есть 18?","17");
+// console.log(typeof(answer));
+// console.log(answer+5);
+// const answers = [];
+// answers [0] = prompt ('Как ваше имя?', '');
+// answers [1] = prompt ('Как ваше фамилия?', '');
+// answers [2] = prompt ('Сколько вам лет?', '');
+
+// document.write(answers);
+// console.log(typeof (answers));
+// console.log(typeof (null));
+
+/*
+const category = 'toys';
+// console.log('https://someurl.com/' + category +'/' + '4');
+console.log(`https://someurl.com/${category}/5`);    //Косые ковычки!
+const user ="Norman";
+alert(`Привет, ${user}`);
+*/
+console.log('arr' + " - object");
+console.log(4 + " 5");
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+    
+    console.log(++incr);
+    console.log(--decr);
+
+    // console.log(incr++);
+    // console.log(decr--);
+    // console.log(incr);
+    // console.log(decr);
+    console.log(5%2);
+    // console.log(2*4 === 8);
+    // console.log(2 + 2 *2 ===8);
+    // console.log(2 + 2 *2 !==8);
+    console.log(2 + 2 *2 =='6');
+
+    // && оператор И
+    // || оператор или
+    const   isChecked = true,
+    isClose = true;
+    // console.log(isChecked && isClose);
+    // console.log(isChecked || isClose);
+    console.log(isChecked && !isClose);
+    console.log("-------------------");
+
+    
+    
